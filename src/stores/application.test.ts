6cf56fa311124eb99e7b4c9a9bffc3e98b01a2a8
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2021-05-25 01:12:24 -0500

    patch: integrate primary contact form into app store

--- src/stores/application.test.ts
+++ src/stores/application.test.ts
@@ -1,8 +1,15 @@
 import App, { Step } from './application';
+import PrimaryContactForm from './primary-contact-form';
 
 test('has a default step of 1', () => {
   const app = new App();
   expect(app.step).toBe(1);
+});
+
+test('has fields that equal those of a primary contact form', () => {
+  const app = new App();
+  const form = new PrimaryContactForm();
+  expect(app.fields).toEqual(form.fields);
 });
 
 describe('setStep', () => {
--- src/stores/application.ts
+++ src/stores/application.ts
@@ -1,9 +1,18 @@
 import { makeObservable, observable, action } from 'mobx';
+import PrimaryContactForm from './primary-contact-form';
 
 export type Step = 1 | 2 | 3 | 4;
 
 class App {
   public step: Step = 1;
+
+  public primaryContactForm: PrimaryContactForm = new PrimaryContactForm();
+
+  public get fields(): Record<string, unknown> {
+    return {
+      ...this.primaryContactForm.fields,
+    };
+  }
 
   setStep(step: Step): void {
     this.step = step;
@@ -12,6 +21,7 @@ class App {
   constructor() {
     makeObservable(this, {
       step: observable,
+      primaryContactForm: observable,
       setStep: action.bound,
     });
   }
