e3f1a14c3ee273bb36dc66a8e05ed2070a985cca
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2021-05-24 20:09:06 -0500

    patch: css layout

---
+++ src/pages/background.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: block;
+  width: 100%;
+  background-color: #fbfbfc;
+`;
+
+export default Container;
---
+++ src/pages/container.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: block;
+  width: 100%;
+  max-width: 600px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export default Container;
