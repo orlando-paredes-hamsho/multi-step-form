2f0264c4396c4e089d1d89207c337179f664b553
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2021-05-24 19:12:11 -0500

    patch: second card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import PrimaryContact from './primary-contact';
+// import PrimaryContact from './primary-contact';
+import AboutYourCompany from './about-your-company';
 
 export default function IndexPage(): JSX.Element {
-  return <PrimaryContact />;
+  return <AboutYourCompany />;
 }
