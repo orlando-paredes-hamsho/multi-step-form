e96bc332b6c8da00238816eb79c9f0aa96c427e1
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2021-05-24 21:54:29 -0500

    patch: add app controller

---
+++ src/stores/application.ts
@@ -0,0 +1,21 @@
+import { makeObservable, observable, action } from 'mobx';
+
+type Step = 1 | 2 | 3 | 4;
+
+class App {
+  step: Step = 1;
+
+  setStep(step) {
+    this.step = step;
+  }
+
+  constructor() {
+    makeObservable(this, {
+      step: observable,
+      setStep: action.bound,
+    });
+  }
+}
+
+export default App;
+export const app = new App();
